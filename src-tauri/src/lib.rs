7cb8861142fb9086c864aa8e0262042890a42411
WiseYaroslav28 <wise.yaroslav@gmail.com>
2026-06-14 02:19:29 +0300

    feat: integrate tauri updater and process plugins with settings UI

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4952,6 +4952,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
             tauri_plugin_global_shortcut::Builder::new()
                 .with_shortcuts(["alt+q"])
